1fb8f9e19f020c20771a85ec9775cf950568843d
kazuya.makida@rakus.co.jp <cullmi-kazuya>
2024-06-05 22:47:41 +0900

    Initial Commit

---
+++ homePortal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homePortal
+//
+//  Created by 牧田和也 on 2024/06/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
